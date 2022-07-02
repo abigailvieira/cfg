08e590acbc99232bdd5e6393a3476336103680fd
abigail! <abigailvieira@hotmail.co.uk>
2022-07-02 13:52:27 +0100

    added remoted connection instructions

---
+++ example.py
@@ -0,0 +1,15 @@
+# GIT FLOW
+# 1. git add <filename>
+# 2. git commit <filename> -m "my message"
+# 3. 
+
+# MISC COMMANDS
+# git status
+# git log
+# git checkout <hash> (timetravel)
+# git switch - (undo timetravel)
+
+# SET UP REMOTE CONNECTION
+# git remote add origin https://github.com/abigailvieira/cfg.git
+# git remote: double check that there is a new origin
+# git branch -M main (makes new branch)
